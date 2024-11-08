f23265593e873f3ccf03fc37cce906be7ed52394
Tala Awwad <awwadtala08@gmail.com>
2024-11-08 13:57:12 -0800

    tables created in schema

---
+++ db/schema.sql
@@ -0,0 +1,23 @@
+CREATE DATABASE sql12;
+
+\c sql12;
+
+CREATE TABLE department (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE role (
+id SERIAL PRIMARY KEY,
+name VARCHAR(30) UNIQUE NOT NULL,
+salary DECIMAL NOT NULL,
+department_id INTEGER NOT NULL
+);
+
+CREATE TABLE employee (
+id SERIAL PRIMARY KEY,
+first_name VARCHAR(30) NOT NULL,
+last_name VARCHAR(30) NOT NULL,
+role_id INTEGER NOT NULL,
+manager_id INTEGER 
+);
